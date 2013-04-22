46876077390912ab3fb4d86a80508bbf9656cd39
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2013-04-22 16:56:23 +0200

    Add jQuery example test

---
+++ tasks/jquery.js
@@ -0,0 +1,12 @@
+tasks.add("find-body", {
+  instruction: 'Save jQuery object for body into body variable',
+  js: "var body = ?;",
+  html: "",
+  tests: function(){
+    test("is it there?", function(){
+      ok(body !== undefined, "body variable defined");
+      strictEqual(body.length, 1, "body contains one element...");
+      strictEqual(body[0], document.body, "...which is body");
+    });
+  }
+});

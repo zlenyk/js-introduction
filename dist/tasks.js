f37f700a00927ecb5664f917e256eaf01740b89f
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2013-05-15 22:36:32 +0200

    Update tasks.js

--- dist/tasks.js
+++ dist/tasks.js
@@ -800,6 +800,29 @@ tasks.add('returning-object', {
 
 tasks.module("homework");
 
+tasks.add("prime-numbers", {
+  instruction: "Write a function `f` with takes argument `n` and returns object `{left: x, right: y}` where `x` is the largest prime number less than `n` and `y` is the smallest prime number greater than `n`. If `x` (or `y`) does not exists `x` then `x = -1` (or `y = -1`).",
+  js: "var f = function(n){\n\n}",
+  tests: function(){
+    test('test f', function(){
+      // for testing functions see: http://api.qunitjs.com/category/assert/
+      notStrictEqual(f, undefined, 'f is defined');
+      strictEqual(typeof f, 'function', 'f is a function');
+
+
+      deepEqual(f(10), {left: 7, right: 11}, 'integer');
+      deepEqual(f(1), {left: -1, right: 2}, 'small integer');
+      deepEqual(f(-5), {left: -1, right: 2}, 'negative integer');
+      deepEqual(f(94322), {left: 94321, right: 94327}, 'big integer');
+      deepEqual(f('test'), {left: -1, right: -1}, 'string');
+      deepEqual(f(20.5), {left: 19, right: 23}, 'float');
+      deepEqual(f(2.1), {left: 2, right: 3}, 'float2');
+    });
+  }
+});
+
+tasks.module("homework");
+
 tasks.add("sum-zero", {
   instruction: "Write a function `f` it takes array `n`. Divide it in two other arrays first one there are elements whose don't sum to zero with other elements, the other one there are pair of elements which sum to zero. The order of elements should be like in input array. Return object with properities `rest` where are not \"zero-pair\" element and `zero` where are \"zero-pair\" elements. For example [3, 1, 2, 4, -2, -1] answer is {rest:[3, 4], zero:[1, -1, 2, -2]}. Number 0 isn't in any array. If in the array has different type than numeric return -1",
   js: "var f = function(n){\n\n}",
